2b6e2ab4e71c6dfe4d2be6d5442fee7b55e7e1c3
MAHESH <maheshshinde9100@gmail.com>
2025-06-28 12:58:37 +0530

    Tested Redis DB Successfully

--- src/main/java/com/mahesh/fetchivo/FetchivoApplication.java
+++ src/main/java/com/mahesh/fetchivo/FetchivoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +20,13 @@ public class FetchivoApplication {
 		SpringApplication.run(FetchivoApplication.class, args);
 	}
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	@PostConstruct
-	public void postConstruct(){
-		System.out.println("POST CONSTRUCT called - Spring Boot Application Class");
+	public void testRedis() {
+		redisTemplate.opsForValue().set("test-key", "Mahesh");
+		String value = (String) redisTemplate.opsForValue().get("test-key");
+		System.out.println("Redis value: " + value);
 	}
 }
